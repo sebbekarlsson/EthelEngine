02e175d9c9c8b783fe6d900eaa69208949f30553
= <sebbekarlsson97@gmail.com>
2015-05-02 13:11:48 +0200

    Inits graphics

--- src/ethel/main/Ethel.java
+++ src/ethel/main/Ethel.java
@@ -1,10 +1,13 @@
 package ethel.main;
 
+import java.awt.Dimension;
+
 public class Ethel {
 	
 	public static int WIDTH = 640;
 	public static int HEIGHT = WIDTH / 16 * 9;
 	public static int SCALE = 2;
+	public static Dimension DISPLAY_SIZE = new Dimension(WIDTH * SCALE, HEIGHT * SCALE);
 	public static String TITLE = "Game";
 	public static int SYNC_FPS = 60;
 	
