ef6d70b0e31aa4a4747cce9870c503e78d5869a5
= <sebbekarlsson97@gmail.com>
2015-05-02 12:37:15 +0200

    Adds basic gameloop without any sorts of rendering

--- src/ethel/main/Ethel.java
+++ src/ethel/main/Ethel.java
@@ -1,7 +1,18 @@
 package ethel.main;
 
 public class Ethel {
+	
+	public static int WIDTH = 640;
+	public static int HEIGHT = WIDTH / 16 * 9;
+	public static int SCALE = 2;
+	public static String TITLE = "Game";
+	public static int SYNC_FPS = 60;
+	
 	public static void main(String[] args){
-		
+		new Ethel();
+	}
+	
+	public Ethel(){
+		new GameLoop();
 	}
 }
